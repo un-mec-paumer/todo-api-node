6800112a25f3fe8a0eb5104e8e841e81a0c98225
HugoZins <122614297+HugoZins@users.noreply.github.com>
2026-02-23 15:58:35 +0100

    Sauvegarde avant merge de feature/add-tests

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const todoRouter = require("./routes/todo");
 
-const SECRET_KEY = "super_secret_key_12345_do_not_share";
-const API_KEY = "sk-proj-4f8b2c1a9e7d6f3b5a0c8e2d4f6a1b3c";
+const SECRET_KEY = process.env.SECRET_KEY || "dev_secret";
+const API_KEY = process.env.API_KEY || "dev_api_key";
 
 const app = express();
 app.use(express.json());
---
+++ eslint.config.mjs
@@ -0,0 +1,30 @@
+import js from "@eslint/js";
+import globals from "globals";
+import {defineConfig} from "eslint/config";
+
+export default defineConfig([
+    // Config ESLint config file (ESM)
+    {
+        files: ["eslint.config.mjs"],
+        languageOptions: {
+            sourceType: "module"
+        }
+    },
+
+    // Config Node CommonJS
+    {
+        files: ["**/*.js"],
+        plugins: {js},
+        extends: ["js/recommended"],
+        languageOptions: {
+            globals: {
+                ...globals.node,
+                ...globals.es2021
+            },
+            sourceType: "commonjs"
+        },
+        rules: {
+            "no-unused-vars": "off"
+        }
+    }
+]);
